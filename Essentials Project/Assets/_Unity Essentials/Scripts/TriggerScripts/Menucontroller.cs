e95a0d065f1ef994f6f29c2fbb04ba4671ee3bbe
lol <strasinski7@gmail.com>
2026-01-14 10:17:54 +0100

    menu & sounds 1

---
+++ Essentials Project/Assets/_Unity Essentials/Scripts/TriggerScripts/Menucontroller.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class Menucontroller : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+    private bool musicMuted = false;
+    private bool sfxMuted = false;
+    public GameObject menu;
+    private bool menuActive = false;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
