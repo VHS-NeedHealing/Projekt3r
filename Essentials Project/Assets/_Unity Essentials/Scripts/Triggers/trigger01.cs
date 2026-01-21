1389deca1128dff542fd084bdb1d08a32892f478
VHS-NeedHealing <p.biesek.pb@gmail.com>
2026-01-21 09:20:37 +0100

    Fixed some stuff

---
+++ Essentials Project/Assets/_Unity Essentials/Scripts/Triggers/trigger01.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class trigger01 : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("uuuuuuuu");
+
+       
+    }
+}
